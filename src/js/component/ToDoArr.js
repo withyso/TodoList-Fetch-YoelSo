0a8bff07e0fc831885aa974275097708f859cec6
Adrian <142854436+withyso@users.noreply.github.com>
2024-04-17 15:40:59 +0000

    fetch function done, issue with multiple request pending

--- src/js/component/ToDoArr.js
+++ src/js/component/ToDoArr.js
@@ -5,9 +5,9 @@ export const ToDoArr = ({ tarea, funcionDeBorrado }) => {
     return (
         <div>
             <ul>
-                <li className="list-group-item liBox ps-4 fs-5 text-dark p-1"> {tarea.name}
+                <li className="list-group-item liBox ps-4 fs-5 text-dark p-1"> {tarea.label}
                     <i className="fa-solid fa-x pe-4" onClick={() => {
-                        funcionDeBorrado(tarea.name)
+                        funcionDeBorrado(tarea.label)
                     }}> </i> </li>
             </ul>
         </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,14 +13,26 @@ import { ToDoArr } from "./ToDoArr";
 
 //create your first component
 const Home = () => {
-	const [taskData, setTaskData] = useState([{ name: 'Make your bed', id: crypto.randomUUID() }, { name: 'Do your Homework', id: crypto.randomUUID() }])
+
+	//Fetch and Api functions
+	const getURL = "https://playground.4geeks.com/todo/"
+	fetch(getURL + "users/yoelwithy")
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data)
+			setTaskData(data.todos)
+		})
+		.catch((error) => { return error })
+
+	const [taskData, setTaskData] = useState([])
 	const [inputData, setInputData] = useState('');
 	let arrAmmount = taskData.length;
 
+	//Handle local data and Array functions
 	const handleKeydown = (e) => {
 		if (e.key == 'Enter' && inputData !== '') {
 			console.log(`la tarea se ha enviado satisfactoriamente con el valor: ${inputData}`)
-			setTaskData([...taskData, { name: inputData, id: crypto.randomUUID() }])
+			setTaskData([...taskData, { label: inputData, id: crypto.randomUUID() }])
 			setInputData('')
 		}
 		else console.log("Aun no se envia la tarea o no se presiona Enter")
@@ -42,7 +54,7 @@ const Home = () => {
 		console.log('procesando solicitud de borrado')
 		console.log('arreglo encontrado.., Valor es igual a:', taskName)
 		let copyList = [...taskData];
-		let newList = copyList.filter((task) => task.name !== taskName);
+		let newList = copyList.filter((task) => task.label !== taskName);
 		setTaskData(newList)
 		console.log(newList)
 	}
